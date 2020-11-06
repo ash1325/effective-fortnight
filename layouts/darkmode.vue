10611d69bdc3d2d742ffd3d280f66051cf09fbf8
k-harada <k-harada@geo-brain.com>
2020-11-06 17:39:24 +0900

    a

---
+++ layouts/darkmode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dark">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  body{
+    background: #000;
+    color: #fff;
+  }
+
+  .dark{
+    background: #000;
+    color: #fff;
+  }
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <nuxt-link to="/">TOP</nuxt-link>
+</template>
+
+<script>
+export default {
+  head(){
+    return {
+      title: 'About',
+      meta: [
+        { hid: 'description', name: 'description', content: 'About ページ' }
+      ]
+    }
+  },
+  layout: 'darkmode'
+}
+</script>
+
+<style lang="scss">
+
+</style>
